77e5a4f673c8ba1667015c6f785e55742050fc14
Karen Elenjickal <20465378+Langlife@users.noreply.github.com>
2021-02-18 16:48:26 -0500

    Add files via upload

--- client_ke2.py
+++ client_ke2.py
@@ -8,23 +8,12 @@ import csv
 
 def client():
 
-    #fileObject = open("/ilab/users/kje42/project1/PROJI-DNSRS.txt", "r")
-    #data = fileObject.read()
-    #print(data)
-
     try:
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
     except socket.error as err:
         print('socket open error: {} \n'.format(err))
         exit()
-
-
-    # Define the port on which you want to connect to the server
-    #port = 50007
-
-    #get root server hostname, root server listen port, and ts listen port
-    #from command line arguments
     rsHostname = sys.argv[1]
     rsListenPort = sys.argv[2]
     tsListenPort = sys.argv[3]
@@ -32,37 +21,28 @@ def client():
     print("rsListenPort " + rsListenPort)
     print("tsListenPort " + tsListenPort)
     localhost_addr = socket.gethostbyname(socket.gethostname())
-
-
-    # connect to the server on local machine
     server_binding = (rsHostname, int(rsListenPort))
-    #server_binding = (localhost_addr, int(rsListenPort))
     cs.connect(server_binding)
-
-    #get info from text file
-
-    #get file object
     f = open("./PROJI-HNS.txt", "r")
+    file = open("./RESOLVED.txt", "w")
+    count1 = len(open("/ilab/users/kje42/project1/PROJI-HNS.txt").readlines(  ))
+    print("COUNT1") 
+    print(count1)
     while(True):
-	#read next line
+    #read next line
         line = f.readline()
-        #if line is empty, you are done with all lines in the file
         if not line:
             break
-        #you can access the line
-        #print(line.strip())
         address1 = ''
-        print("Client sent:" + address1)
         for element in range(0, len(line)):
                address1 += line[element]
         cs.send(address1.encode('utf-8'))
+        print("Client sent:" + address1)
         data_from_server=cs.recv(100)
-        #100 bytes of message it can receive
         print("[C]: Data received from root server: {}".format(data_from_server.decode('utf-8')))
         received_data = data_from_server.decode('utf-8')
-        file = open("./RESOLVED.txt", "w") 
         file.write(received_data) 
-        file.close() 
+     
 
         # This is the case where the client recieved a message from
         # the root server indicating that the IP address is not known by the root
@@ -75,8 +55,7 @@ def client():
                 print("[C]: Client-TS socket created")
             except socket.error as err:
                 print('socket open error: {} \n'.format(err))
-                exit()
-       
+                exit()       
             #parse out ts hostname from response
             # and create client/TS socket from it
             tsHostname = received_data.split(' ',1)[0]
@@ -91,17 +70,8 @@ def client():
             #write response into RESOLVED.txt file
             file = open("./RESOLVED.txt", "w") 
             file.write(data_from_TSserver) 
-            file.close() 
-    
-    #close file
+    file.close()      
     f.close
-
-    # Receive data from the server if the host is in the dictionary
-    #data_from_server=cs.recv(100)
-    #100 bytes of message it can receive
-    #print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
-
-    # close the client socket
     cs.close()
     exit()
 
